fe805850c81c5823ed981e195bc76ba829c19589
MaxGirouard <max.girouard@gmail.com>
2025-03-30 15:06:11 -0400

    New ideas for data wrangling

--- code.R
+++ code.R
@@ -1,3 +1,10 @@
 # Documentation
 library(tidyverse)
 library(psych)
+
+# To add cases to an existing data file, use instead
+union()
+add_case() |> distinct()
+
+# To keep only commom cases use 
+intersect()
